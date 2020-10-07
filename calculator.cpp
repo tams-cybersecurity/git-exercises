19156bd18a0ed52f04e53df29c5f07199e175788
Daniel Ting <danielzting@gmail.com>
2020-10-07 15:06:07 -0500

    Initial commit

---
+++ calculator.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Type +, -, or *: ";
+    char op;
+    std::cin >> op;
+    std::cout << "Type a: ";
+    int a;
+    std::cin >> a;
+    std::cout << "Type b: ";
+    int b;
+    std::cin >> b;
+    int c;
+    switch (op)
+    {
+        case '+':
+            c = a + b;
+            break;
+        case '-':
+            c = a + b;
+            break;
+        case '*':
+            c = a * b;
+            break;
+    }
+    std::cout << a << op << b << '=' << c << std::endl;
+    return 0;
+}
